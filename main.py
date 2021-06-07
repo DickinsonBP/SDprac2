c058d4ef738dd1e15f322298a896334c46fc7edf
Dickinson Bedoya Perez <dickinsonbp@gmail.com>
2021-06-07 20:09:26 +0200

    config ibmcloud --> anna gracia

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-#from multiprocessing import Pool
+import pandas
 from lithops.multiprocessing import Pool
-from lithops import FunctionExecutor
+from lithops import Storage, FunctionExecutor
 
 REGION = 'eu-de'
 NAMESPACE = 'anna.graciac@estudiants.urv.cat_dev'
@@ -10,21 +9,14 @@ ACCESS_KEY_ID = '21d22b43d16f4d19b6e5ed4ff9eebaad'
 SECRET_ACCES_KEY = '1ec74d1f3d926d28de2cd6eda3fbbb526c50dfe9840d837e' 
 BUCKET = 'covid-dataset'
 ENDPOINT = 'https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints'
+FILE_CSV = 'covid-muni-sex.csv'
 
-
-
-def f(x):
-    time.sleep(10)
-    return x*x
-
-
-if __name__ == '__main__':
-    with Pool() as p:
-        print(p.map(f,range(100)))
-
-'''def hello(name):
-    return 'Hello {}!'.format(name)
-
-with FunctionExecutor() as fexec:
-    future = fexec.call_async(hello,'World')
-    print(future.result())'''
+#configuracion de lithops
+conf = {'lithops':{'storage_bucket':BUCKET},
+        'ibm_cf':{'endpoint':ENDPOINT,
+                    'namespace':NAMESPACE,
+                    'api_key':API_KEY},
+        'ibm_cos':{'region':REGION,
+                    'acces_key':ACCESS_KEY_ID,
+                    'secret_key':SECRET_ACCES_KEY}}
+#leer archivo
