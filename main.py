73801e14edcbe3146256a72591ae780224a01d12
Anna Gracia <anna.gracia@outlook.com>
2021-06-08 13:07:06 +0200

    arreglo config

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ REGION = 'eu-de'
 NAMESPACE = 'anna.graciac@estudiants.urv.cat_dev'
 API_KEY = "2e728aba-560e-40f1-988b-6965b5ea4579:UDIJT2tLhxF5YKhLi00f1OWFNVN1XSpH1y9SMpJPyUZmPBSDsRMwixt6Qre2YThc"
 ACCESS_KEY_ID = '21d22b43d16f4d19b6e5ed4ff9eebaad'
-SECRET_ACCES_KEY = '1ec74d1f3d926d28de2cd6eda3fbbb526c50dfe9840d837e' 
+SECRET_ACCESS_KEY = '1ec74d1f3d926d28de2cd6eda3fbbb526c50dfe9840d837e' 
 BUCKET = 'covid-dataset'
 ENDPOINT = 'https://eu-de.functions.cloud.imb.com'
 KEY = 'covid-cat.csv'
@@ -15,11 +15,11 @@ KEY = 'covid-cat.csv'
 #configuracion de lithops
 config = {'lithops' : {'storage_bucket' : BUCKET},
         'ibm_cf' : {'endpoint' : ENDPOINT, 'namespace' : NAMESPACE, 'api_key' : API_KEY},
-        'ibm_cos' : {'region' : REGION, 'acces_key' : ACCESS_KEY_ID, 'secret_key' : SECRET_ACCES_KEY}}
+        'ibm_cos' : {'region' : REGION, 'access_key' : ACCESS_KEY_ID, 'secret_key' : SECRET_ACCESS_KEY}}
 #leer archivo
 def obtener_file(key, config):
     dicc = {}
-    storage = Storage(config=config)
+    storage = Storage()
     
     #obtener dict lithops de conf para obtener el bucket name
     lithp = config["lithops"]
