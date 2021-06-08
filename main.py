d75156069521fa7052aef8a66208affd66d45d32
Anna Gracia <anna.gracia@outlook.com>
2021-06-08 12:32:13 +0200

    cambios obtener fichero

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import pandas
+import lithops
 from lithops.multiprocessing import Pool
 from lithops import Storage, FunctionExecutor
 
 REGION = 'eu-de'
 NAMESPACE = 'anna.graciac@estudiants.urv.cat_dev'
-API_KEY = "7GRiz9Ifl-8di9MZF9kP1IAci8M7kk0ytrJAGJrKjs0c"
+API_KEY = "2e728aba-560e-40f1-988b-6965b5ea4579:UDIJT2tLhxF5YKhLi00f1OWFNVN1XSpH1y9SMpJPyUZmPBSDsRMwixt6Qre2YThc"
 ACCESS_KEY_ID = '21d22b43d16f4d19b6e5ed4ff9eebaad'
 SECRET_ACCES_KEY = '1ec74d1f3d926d28de2cd6eda3fbbb526c50dfe9840d837e' 
 BUCKET = 'covid-dataset'
-ENDPOINT = 'https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints'
-FILE_CSV = 'covid-muni-sex.csv'
+ENDPOINT = 'https://eu-de.functions.cloud.imb.com'
+KEY = 'covid-cat.csv'
 
 #configuracion de lithops
-conf = {'lithops':{'storage_bucket':BUCKET},
-        'ibm_cf':{'endpoint':ENDPOINT,
-                    'namespace':NAMESPACE,
-                    'api_key':API_KEY},
-        'ibm_cos':{'region':REGION,
-                    'acces_key':ACCESS_KEY_ID,
-                    'secret_key':SECRET_ACCES_KEY}}
+config = {'lithops' : {'storage_bucket' : BUCKET},
+        'ibm_cf' : {'endpoint' : ENDPOINT, 'namespace' : NAMESPACE, 'api_key' : API_KEY},
+        'ibm_cos' : {'region' : REGION, 'acces_key' : ACCESS_KEY_ID, 'secret_key' : SECRET_ACCES_KEY}}
 #leer archivo
-def obtener_file(archivo, conf):
+def obtener_file(key, config):
     dicc = {}
-    storage = Storage(config=conf)
+    storage = Storage(config=config)
     
     #obtener dict lithops de conf para obtener el bucket name
-    lithp = conf["lithops"]
+    lithp = config["lithops"]
     bucket = lithp["storage_bucket"]
-    archivo = storage.get_object(bucket=bucket, key=file, stream=True)
+    archivo = storage.get_object(bucket=bucket, key=key, stream=True)
 
     #extraer datos del csv y ordenarlos por fecha
     datos = pandas.read_csv(archivo)
@@ -37,10 +34,11 @@ def obtener_file(archivo, conf):
     fecha = datos['TipusCasData'].values_counts()
     dicc['TipusCasData'] = fecha
 
-
     return dicc
 
 
 if __name__ == '__main__':
-    with FunctionExecutor() as fexec:
-        fut = fexec.call_async(obtener_file, (FILE_CSV, conf))
+    with FunctionExecutor(config=config) as fexec:
+        future = fexec.call_async(obtener_file, (KEY, config))
+        dicc = future.result()
+        print(dicc)
