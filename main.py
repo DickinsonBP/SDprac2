e1e08ed303f692e4f68fcecd7fc3e3af71b1efd9
Dickinson Bedoya Perez <dickinsonbp@gmail.com>
2021-06-19 22:47:34 +0200

    borrar config para subir al git

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker
 from collections import OrderedDict
 
-REGION = 'eu-de'
-NAMESPACE = 'anna.graciac@estudiants.urv.cat_dev'
-API_KEY = "2e728aba-560e-40f1-988b-6965b5ea4579:UDIJT2tLhxF5YKhLi00f1OWFNVN1XSpH1y9SMpJPyUZmPBSDsRMwixt6Qre2YThc"
-ACCESS_KEY_ID = '21d22b43d16f4d19b6e5ed4ff9eebaad'
-SECRET_ACCESS_KEY = '1ec74d1f3d926d28de2cd6eda3fbbb526c50dfe9840d837e' 
-BUCKET = 'covid-dataset'
-ENDPOINT = 'https://eu-gb.functions.cloud.ibm.com'
-KEY = ['covid-cat.csv','covid-muni-sex.csv']
-
 #configuracion de lithops
 config = {'lithops' : {'storage_bucket' : BUCKET},
         'ibm_cf' : {'endpoint' : ENDPOINT, 'namespace' : NAMESPACE, 'api_key' : API_KEY},
